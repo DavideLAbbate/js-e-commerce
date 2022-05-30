78024a2c1f92143b6049e28485055321d315fb1f
Davide LAbbate <labbate.davide98@gmail.com>
2022-05-31 00:38:51 +0200

    Adding comments

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ let miniImg1 = document.querySelector(".item1");
 let miniImg2 = document.querySelector(".item2");
 let miniImg3 = document.querySelector(".item3");
 let miniImg4 = document.querySelector(".item4");
+// item mini-img
 
 let piuClick = document.querySelector(".piu");
 let menoClick = document.querySelector(".meno");
+// indicatori + e -
+
+
 let txt = document.querySelector(".txt");
+// quantità
+
+
 let gino = 0;
+
+// cambio immagini on click delle mini img
 
     miniImg1.addEventListener("click", () => {
         gigaImg.classList.add("giga-img1");
@@ -30,6 +39,8 @@ let gino = 0;
         gigaImg.classList.remove("giga-img1" , "giga-img3", "giga-img2");
     })
 
+
+// incremento e decremento della quantità
 
     piuClick.addEventListener("click", () => {
         gino++;
